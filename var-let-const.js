a103b33bc157c5d191365a7bf3b22c5196d5a07f
Daniel Choi <CYDP0127@users.noreply.github.com>
2019-12-09 22:18:30 +0900

    add var-let-const

---
+++ var-let-const.js
@@ -0,0 +1,40 @@
+const example
+const number = 1;
+number = 2;
+
+
+const person = {
+  name: 'Daniel',
+  age: 28,
+};
+person.name = 'Andrew';
+
+const people = []
+people.push(person)
+
+console.log(person, people)
+
+const foo = Object.freeze({
+  'bar': 27
+});
+foo.bar = 42;
+console.log(foo.bar);
+
+console.log(x);
+var x=5;
+console.log(x);
+
+console.log(x);
+let x=5;
+console.log(x);
+
+for(var i=0; i<10; i++){
+  console.log(i);
+}
+console.log(i);
+
+
+for(let j=0; i<10; j++){
+  console.log(j);
+}
+console.log(j);
